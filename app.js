5d2e9b8f15913f81acd6153b9a730ee2de70970a
Cameron Tanjoco <ctanjoco@gmail.com>
2020-12-27 16:14:58 -0800

    Twitter using the MERN stack

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const db = require("./config/keys").mongoURI;
+const mongoose = require("mongoose");
+const app = express();
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require("body-parser");
+const User = require("./models/User");
+const passport = require("passport");
+const path = require("path");
+require("./config/passport")(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log(err));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
